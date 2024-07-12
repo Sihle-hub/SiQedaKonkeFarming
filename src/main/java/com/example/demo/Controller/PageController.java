1e5cf84785dd47ef9d9aad83150f42b92f38f9ff
sihle <sihlesithole810@gmail.com>
2024-07-12 19:10:51 +0200

    SiQedaKonke datasource

---
+++ src/main/java/com/example/demo/Controller/PageController.java
@@ -0,0 +1,16 @@
+package com.example.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+	
+	@GetMapping("/welcome_page")
+	public String welcomePage() {
+		return "welcome_page.html";
+	}
+	
+	
+
+}
